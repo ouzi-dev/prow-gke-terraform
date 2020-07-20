a9f65f28c72cf3a16b2a0db51b8f0040811d2579
Alex Ouzounis <alex@ouzi.dev>
2020-07-20 10:36:57 +0100

    feat: create before destroy node pool (#36)
    Signed-off-by: Alex Ouzounis <alex@ouzi.dev>

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "google_project_service" "project" {
 
 ## Modules
 module "gke-cluster" {
-  source = "github.com/ouzi-dev/gke-terraform.git?ref=v0.9.3"
+  source = "github.com/ouzi-dev/gke-terraform.git?ref=v0.10.0"
   # source  = "../gke-terraform"
   region  = var.gcloud_region
   project = var.gcloud_project
